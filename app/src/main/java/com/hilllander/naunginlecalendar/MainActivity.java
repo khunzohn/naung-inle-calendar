4e035de2a17fd0e7cb2027a43b37a994f1fc6a57
khunzohn <khunzohn@gmail.com>
2015-11-03 15:32:52 +0630

    use CoordinatorLayout and add toolbar

--- app/src/main/java/com/hilllander/naunginlecalendar/MainActivity.java
+++ app/src/main/java/com/hilllander/naunginlecalendar/MainActivity.java
@@ -2,6 +2,7 @@ package com.hilllander.naunginlecalendar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
