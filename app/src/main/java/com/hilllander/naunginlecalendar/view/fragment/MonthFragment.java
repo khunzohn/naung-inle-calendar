e5355b642ba1f5383ba4bb05915526f587f8cddc
khunzohn <khunzohn@gmail.com>
2015-11-11 11:04:07 +0630

    change month grid weekday to unicode

--- app/src/main/java/com/hilllander/naunginlecalendar/view/fragment/MonthFragment.java
+++ app/src/main/java/com/hilllander/naunginlecalendar/view/fragment/MonthFragment.java
@@ -145,7 +145,7 @@ public class MonthFragment extends Fragment {
         cal.set(mYear, mMonth, mDay);
         Log.d(TAG + "createMonthGridItem", cal.get(Calendar.YEAR) + " " + cal.get(Calendar.MONTH) + " " + cal.get(Calendar.DAY_OF_MONTH));
         MyanmarCalendar mCal = MyanmarCalendar.getInstance(cal);
-        MonthGridItem item = MonthGridItem.newInstance()
+        MonthGridItem item = MonthGridItem.newInstance(mCal)
                 .setEngDay(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)))
                 .setMyaDay(mCal.getDayInMyanmnar())
                 .setMyaMonth(mCal.getMonthInMyanmar(getContext()))
