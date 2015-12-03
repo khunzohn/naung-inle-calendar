e164640cc1db5bcec2682729d6fadfdc0598a593
khunzohn <khunzohn@gmail.com>
2015-12-04 00:16:04 +0630

    replaced hilllander's page's id with Naung inle calendar page id

--- app/src/main/java/com/hilllander/naunginlecalendar/util/FacebookOpener.java
+++ app/src/main/java/com/hilllander/naunginlecalendar/util/FacebookOpener.java
@@ -9,10 +9,10 @@ import android.net.Uri;
  */
 public class FacebookOpener {
     public static Intent getOpenFacebookIntent(Context context) {
-
+        /*hilllander page id = 405267716349059 */
         try {
             context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
-            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/405267716349059"));
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/746688995463780")); // naung inle calendar page id
         } catch (Exception e) {
             return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/NicheOfHilllander"));
         }
